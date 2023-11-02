2fa8aa7f54288391852370b0254a02abe67b730a
Şeref Kaynak <serefkynk@gmail.com>
2023-11-02 14:49:41 +0300

    Adding formik

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import React from 'react';
+import { Formik, Field, Form } from 'formik';
+
 
 function App() {
+  {/*- [ ] Takım üyelerini içeren listeyi bir stateten render edin.*/}
+  const [members, setMembers] = useState([]);
+
+
+  {/*  - [ ] Üye listesine yeni bir üye eklemek için bir form oluşturun.*/}
+  const [newMember, setNewMember] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    address: '',
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<h1>Any place in your app!</h1>
+     <Formik
+       initialValues={{ email: '', password: '' }}
+       validate={values => {
+         const errors = {};
+         if (!values.email) {
+           errors.email = 'Required';
+         } else if (
+           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+         ) {
+           errors.email = 'Invalid email address';
+         }
+         return errors;
+       }}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+       {({ isSubmitting }) => (
+         <Form>
+           <Field type="email" name="email" />
+           {/*<ErrorMessage name="email" component="div" />*/}
+
+            <br>
+            </br>
+
+           <Field type="password" name="password" />
+           {/*<ErrorMessage name="password" component="div" />*/}
+
+            <br>
+            </br>
+
+           <button type="submit" disabled={isSubmitting}>
+             Submit
+           </button>
+         </Form>
+       )}
+     </Formik>
     </div>
   );
 }
